adb086dc132cf139791c6318d9202be41bae3254
angel teyul <willson.alvarado@outlook.com>
2024-01-16 21:17:41 -0600

    corrected code to start the server and import dependencies, added project structure folders as well

---
+++ README.md
@@ -0,0 +1,18 @@
+# API created using the version 3 of The Movie Database (TMDB) API
+
+### How to use it
+
+After you have downloaded or cloned the GitHub repository, install the dependencies using the following command:
+
+```shell
+npm install
+```
+
+To run the server use the following command:
+
+```shell
+npm start
+```
+
+> By default the port used is 3000, but feel free to change it as you please.
+
--- api.js
+++ api.js
@@ -1,10 +1,41 @@
+// import dependencies needed
+
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config()
+
 // creating the server
 
-const express = require('express')
-const app = express()
-const PORT = 3000
+const app = express();
+const port = process.env.SERVER_PORT;
 
+// encode characters correctly if needed when requesting JSON data
 
-// open server to listen to requests and show a message to confirm it's running
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(PORT, () => console.info(`Server listening on port: ${PORT}`))
+// configure engine view (EJS) and where the views are stored
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// configure root directory
+
+app.use(express.static(__dirname + '/public'));
+
+// open server to listen to requests
+
+app.listen(port, () => {
+    console.log('Listening on port:', port);
+})
+
+// render index (main page)
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+// page not found
+
+app.use((req, res) => {
+    res.status(404).send('Page not found');
+})
